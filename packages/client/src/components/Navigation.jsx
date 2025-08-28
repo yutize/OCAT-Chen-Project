601fff6f906c27ce66a062c04cd3d6f202f8af45
yutize <chen6yt@mail.uc.edu>
2025-08-28 11:55:12 -0400

    Redone nav links
    Originally the links do not go to the correct route

--- packages/client/src/components/Navigation.jsx
+++ packages/client/src/components/Navigation.jsx
@@ -9,7 +9,7 @@ export const Navigation = () =>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
+            <Nav.Link as={Link} to="/assessment/list">Dashboard</Nav.Link>
             <Nav.Link as={Link} to="/assessment/new">New Assessment</Nav.Link>
           </Nav>
         </Navbar.Collapse>
